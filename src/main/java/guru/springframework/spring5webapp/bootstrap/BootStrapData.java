f0fb237a958f8adfb3c35b51b74dd3f6642e6d5b
Lukas Molcan <lukas.molcan@gmail.com>
2020-05-30 17:49:53 +0200

    New Entities and Repositories + initializing data

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,53 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.entity.Author;
+import guru.springframework.spring5webapp.entity.Book;
+import guru.springframework.spring5webapp.entity.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner{
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author a1 = new Author("Eric","Evans");
+        Book b1 = new Book("Domain Driven Design","123123");
+
+        a1.getBookSet().add(b1);
+        b1.getAuthorSet().add(a1);
+
+        authorRepository.save(a1);
+        bookRepository.save(b1);
+
+        Author a2 = new Author("Rod","Johnson");
+        Book b2 = new Book("J2EE Development without EJB","3939459459");
+
+        a2.getBookSet().add(b2);
+        b2.getAuthorSet().add(a2);
+
+        authorRepository.save(a2);
+        bookRepository.save(b2);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: "+bookRepository.count());
+
+        Publisher p1 = new Publisher("Lukas Molcan Company","Odbojarska 9","Bratislava","Slovakia",98765);
+        publisherRepository.save(p1);
+
+        System.out.println("Number of Publishers: "+publisherRepository.count());
+    }
+}
